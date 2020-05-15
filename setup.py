970e4dbdc891fb5b1dc706d2287d2b11de121fa5
morioprog <morio.prog@gmail.com>
2020-05-15 23:47:33 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name="dc-debug",
     version="0.0.1",
+    packages=find_packages(exclude=('test')),
     entry_points={
         "console_scripts": [
             "dc-debug = src.main:main"
